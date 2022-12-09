8b40326b32b14cd7fcd5e167a88a1d7edd023326
C0dingChris <Chris.Sanger@outlook.com>
2022-12-09 14:16:13 +0000

    Set up useContext for theme change

--- login-calendar/src/Components/App/App.js
+++ login-calendar/src/Components/App/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import LogoutButton from "../LogoutButton/LogoutButton";
 import LoginButton from "../LoginButton/LoginButton";
@@ -7,13 +7,29 @@ import Profile from "../LoginProfile/LoginProfile";
 import "./App.css";
 import CalendarComp from "../Calendar/Calendar";
 
-function App() {
-  return (
-    <div className="App">
-      <LoginButton />
+export const ThemeContext = React.createContext();
 
-      <Profile />
-      <CalendarComp />
+function App() {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  function toggleBG() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+
+  const themeStyles = {
+    backgroundColor: darkTheme ? "lightblue" : "orange",
+  };
+
+  return (
+    <div className="App" style={themeStyles}>
+      <ThemeContext.Provider value={darkTheme}>
+        <LoginButton />
+        <button className="fourth" onClick={toggleBG}>
+          Toggle Theme
+        </button>
+        <Profile />
+        <CalendarComp />
+      </ThemeContext.Provider>
     </div>
   );
 }
--- login-calendar/src/Components/Calendar/Calendar.js
+++ login-calendar/src/Components/Calendar/Calendar.js
@@ -4,15 +4,24 @@ import "./Calendar.css";
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Time from "../Time/Times";
+import { useContext } from "react";
+import { ThemeContext } from "../App/App";
 
 const CalendarComp = () => {
   const [date, setDate] = useState(new Date());
   const [showTime, setShowTime] = useState(false);
   const { user, isAuthenticated } = useAuth0();
+  const darkTheme = useContext(ThemeContext);
+
+  const themeStyles = {
+    backgroundColor: darkTheme ? "white" : "black",
+    color: darkTheme ? "black" : "white",
+  };
+  console.log(date);
   return (
     isAuthenticated && (
       <div>
-        <div className="header-container">
+        <div className="header-container" style={themeStyles}>
           <h1 className="header">{user.given_name}'s Calendar</h1>
         </div>
         <div className="calendar-parent">
